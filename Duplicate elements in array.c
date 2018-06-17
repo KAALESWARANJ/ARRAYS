033561429707499321d46ef11a7fa4188e8e31de
KAALESWARANJ <34641557+KAALESWARANJ@users.noreply.github.com>
2018-06-17 12:51:46 +0530

    Create Duplicate elements in array.c

---
+++ Duplicate elements in array.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[1000],i,j,n,count=0, min;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        min = i;
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[min]>arr[j])
+            {
+                min = j;    
+            }
+        }
+        {
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]==arr[i-1])
+        {
+            count++;
+            while(arr[i]==arr[i-1]) i++;
+        }
+    }
+    printf("%d",count);
+
+    return 0;
+}
